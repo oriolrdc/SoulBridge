cf5ee457c854a0579cd79dc3628254dc739dcb26
oriolrdc <oroca@ctsformacion.com>
2026-03-02 20:34:47 +0100

    Movement+Chang+Dash (FIXED)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,68 +7,48 @@ public class PlayerController : MonoBehaviour
     #region Variables
     //Components
     private CharacterController _controller;
-    private Animator _animator;
     //Inputs
     private InputAction _moveAction;
     private InputAction _lookAction;
-    private InputAction _attackAction;
-    private InputAction _dashAction;
-    private InputAction _interactAction;
     private InputAction _changeAction;
-    //InputDependences
-    private Vector2 _moveInput;
+    private InputAction _dashAction;
     private Vector2 _lookInput;
-    //Gravity
-    [SerializeField] private Transform _sensorPosition;
-    [SerializeField] private float _sensorRadius;
-    [SerializeField] private LayerMask _groundLayer;
-    [SerializeField] private float _gravity = -9.81f;
-    [SerializeField] Vector3 _playerGravity;
+    private Vector2 _moveInput;
     //Stats
     [SerializeField] private float _movementSpeed = 5;
     [SerializeField] private float _smoothTime = 0.1f;
-    private float _turnSmoothVelocity;
-    //Camera
-    private Transform _mainCamera;
-    float targetAngle;
-    //CharacterChange
-    [SerializeField] private GameObject _Chico;
-    [SerializeField] private GameObject _Chica;
-    [SerializeField] private bool _ChicoActive = false;
-    [SerializeField] private bool _ChicaActive = true;
-    private bool _isChanging = false;
+    //Cedric
+    [SerializeField] GameObject _Cedric;
+    [SerializeField] bool _CAct;
+    //Thalya
+    [SerializeField] GameObject _Thalya;
+    [SerializeField] bool _TAct;
     //Dash
     [SerializeField] private float _dashSpeed = 20;
     private bool _dashing = false;
     float _dashTimer;
     [SerializeField] float _dashDuration = 0.5f;
-    //ThalyaAttack
-    //
-    //CedricAttack
-    //
+    //Dependences
+    private bool _isChanging;
+    private float _turnSmoothVelocity;
+    private float targetAngle;
+    //Gravity
+    [SerializeField] private Transform _groundSensor;
+    [SerializeField] private float _groundSensorRadius;
+    [SerializeField] private LayerMask _groundLayer;
+    private float _gravity = -9.81f;
+    Vector3 _playerGravity;
     
     #endregion
     #region Awake
-    
+
     void Awake()
     {
-        _mainCamera = Camera.main.transform;
         _controller = GetComponent<CharacterController>();
-        _animator = GetComponentInChildren<Animator>();
         _moveAction = InputSystem.actions["Move"];
         _lookAction = InputSystem.actions["Look"];
         _changeAction = InputSystem.actions["Change"];
-        _attackAction = InputSystem.actions["Attack"];
-        _dashAction = InputSystem.actions["Jump"];
-        _interactAction = InputSystem.actions["Interact"];
-    }
-
-    #endregion
-    #region Start
-
-    void Start()
-    {
-        _dashing = false;
+        _dashAction = InputSystem.actions["Dash"];
     }
 
     #endregion
@@ -76,11 +56,53 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        //Inputs
         _moveInput = _moveAction.ReadValue<Vector2>();
         _lookInput = _lookAction.ReadValue<Vector2>();
-        
+        //Functions
         Movement();
+
+        if(_changeAction.WasPressedThisFrame() && _isChanging == false)
+        {
+            _isChanging = true;
+            StartCoroutine(Change());
+        }
+        if(_dashAction.WasPressedThisFrame() && _dashing == false)
+        {
+            StartCoroutine(Dash());
+        }
+    }
+
+    #endregion
+    #region FixedUpdate (Physics)
+
+    void FixedUpdate()
+    {
         Gravity();
+    }
+
+    #endregion
+    #region IsGrounded
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(_groundSensor.position, _groundSensorRadius, _groundLayer);
+    }
+
+    #endregion
+    #region Gravity
+
+    void Gravity()
+    {
+        if(!IsGrounded())
+        {
+            _playerGravity.y += _gravity * Time.deltaTime;
+        }
+        else if(IsGrounded() && _playerGravity.y < 0)
+        {
+            _playerGravity.y = _gravity;
+        }
+        _controller.Move(_playerGravity * Time.deltaTime);
     }
 
     #endregion
@@ -92,6 +114,7 @@ public class PlayerController : MonoBehaviour
 
         Ray ray = Camera.main.ScreenPointToRay(_lookInput);
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
             Vector3 playerForward = hit.point - transform.position;
@@ -109,30 +132,52 @@ public class PlayerController : MonoBehaviour
     }
 
     #endregion
-    #region Gravity
+    #region Change
 
-    void Gravity()
+    IEnumerator Change()
     {
-       if(!IsGrounded())
+        if(_TAct)
         {
-            _playerGravity.y += _gravity * Time.deltaTime;
+            _TAct = false;
+            _CAct = true;
+            _Thalya.SetActive(false);
+            _Cedric.SetActive(true);
+            yield return new WaitForSeconds(1);
+            _isChanging = false;
         }
-        else if(IsGrounded() && _playerGravity.y < 0)
+        else if(_CAct)
         {
-            _playerGravity.y = _gravity;
+            _CAct = false;
+            _TAct = true;
+            _Cedric.SetActive(false);
+            _Thalya.SetActive(true);
+            yield return new WaitForSeconds(1);
+            _isChanging = false;
         }
-        _controller.Move(_playerGravity * Time.deltaTime);
     }
 
     #endregion
-    #region Grounded
+    #region Dash
 
-    bool IsGrounded()
+    IEnumerator Dash()
     {
-        return Physics.CheckSphere(_sensorPosition.position, _sensorRadius, _groundLayer);
+        _dashing = true;
+        Physics.IgnoreLayerCollision(0, 6, true);
+
+        Vector3 dashDirection = new Vector3(_moveInput.x, 0, _moveInput.y).normalized;
+
+        while (_dashTimer < _dashDuration)
+        {
+            
+            _dashTimer += Time.deltaTime;
+            _controller.Move(dashDirection.normalized * _dashSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        Physics.IgnoreLayerCollision(0, 6, false);
+        _dashTimer = 0;
+        _dashing = false;
     }
 
     #endregion
-
-
 }
